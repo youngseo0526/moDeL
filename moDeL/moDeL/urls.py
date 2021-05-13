b07f50baa5906991b2fb4d9fd76696f3bb7921ce
myunbongs <myunbongs@naver.com>
2021-05-14 08:34:16 +0900

    페이지 수정, 이미지 접기 기능, 실시간 비율 측정, 입력 비율과 실시간 비율 비교

--- moDeL/moDeL/urls.py
+++ moDeL/moDeL/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photo.urls')),
+    path('', include('photo.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
